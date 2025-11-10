a31f5391cea3a76f5bfc2501138a4d0c307e35e7
Tanvi <tanvirangray@gmail.com>
2025-11-10 16:04:18 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 11.7 MB (49.09%)

---
+++ solutions/0027-remove-element/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count = 0;
+        for(int i = 0; i<nums.size(); i++)
+        {
+            if(nums[i] != val)
+            {
+                count++;                
+            }
+            else
+            {
+                nums[i] = 100;
+            }
+        }
+
+        std::sort(nums.begin(), nums.end());
+        return count;
+    }
+};
