79143ee689ecdc6785252e460b8cd3b443a7ed90
Tanvi <tanvirangray@gmail.com>
2026-05-14 10:19:04 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 52.4 MB (57.85%)

---
+++ solutions/0055-jump-game/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int size = nums.size();
+        int goal = size-1 ;
+        for(int i = nums.size() - 1 ; i >=0 ; i--){
+            if(i+nums[i] >= goal){
+                goal = i;
+            }
+        }
+
+        if(goal == 0){return true;}
+        else{ return false;}
+    }
+};
