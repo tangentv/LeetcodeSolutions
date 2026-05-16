e6f6b310b91f8748c464e5935064bb6bc54ab8db
Tanvi <tanvirangray@gmail.com>
2026-05-16 07:13:04 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 16.3 MB (61.56%)

---
+++ solutions/0064-minimum-path-sum/solution.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        int ans = solve(grid, m, n);
+        return ans;
+    }
+
+    int solve(vector<vector<int>>& grid,int m, int n)
+    {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        dp[0][0] = grid[0][0];
+
+        for(int i = 1 ;i<m;i++){
+                dp[i][0] = grid[i][0]+ dp[i-1][0];
+        }
+
+        for(int i = 1 ;i<n;i++){
+                dp[0][i] = grid[0][i]+ dp[0][i-1];
+        }
+
+        for(int i = 1;i<m; i++){
+            for(int j=1;j<n;j++){
+
+                int right = grid[i][j] + dp[i-1][j];
+                int down = grid[i][j] + dp[i][j-1];
+                dp[i][j] = min(right, down);
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    
+};
