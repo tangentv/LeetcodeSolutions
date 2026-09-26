2dbe339d38dff52dfe4c119ba1918c3ad5372c01
Tanvi <tanvirangray@gmail.com>
2026-09-26 06:21:58 +0000

    [LeetCode] - Climbing Stairs - Runtime - 2 (4.95%), Memory - 8624000 (19.47%)

--- solutions/0070-climbing-stairs/solution.cpp
+++ solutions/0070-climbing-stairs/solution.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        
-        //base case
+
         if(n==1){return 1;}
         if(n==2){return 2;}
 
-        vector<int> dp(n);
-
+        vector<int>dp(n);
         dp[0] = 1;
         dp[1] = 2;
 
-        for(int i=2; i <n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        for(int i=2;i<n;i++){
+          dp[i] = dp[i-1] + dp[i-2];
         }
-
         return dp[n-1];
-
     }
 };
